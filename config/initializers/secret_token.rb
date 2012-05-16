a9fcabd2c9c3dbd855a79e0e6767550d65a24480
Arin Sime <asime@o19s.com>
2012-05-16 17:41:26 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PersonalFundraisingPages::Application.config.secret_token = '2a2bc1ac59b15c5a74248ba8b94b525a993ada4aa3b7f8d7ee41e647e9dc422f9cc9a12fb93c6a821ccb10fe78176c78ab97c01dbaefe026b00b6af2022b15b4'
