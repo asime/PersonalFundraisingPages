d43ad5b04565f10c5c657eaf24acb8ac962369ff
Arin Sime <asime@o19s.com>
2012-06-28 15:07:16 -0400

    donate button working now and routes for donate action added

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 PersonalFundraisingPages::Application.routes.draw do
   resources :fundraisers
   resources :champions
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
+  match 'fundraisers/:id/donate' => 'fundraisers#donate'
 
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
